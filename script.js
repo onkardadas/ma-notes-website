c83ce4d41f2a38d5c00ab4ac6362b25ea999f25d
onkardadas <167053657+onkardadas@users.noreply.github.com>
2024-04-15 10:09:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const add1 = document.getElementById('i-1');
+const add2 = document.getElementById('i-2');
+const add3 = document.getElementById('i-3');
+const add4 = document.getElementById('i-4');
